82257f9ca0a85577870bafffb4014d7cb4f1417c
Sabaat25 <sabaattungekar25@gmail.com>
2023-07-20 19:48:12 +0530

    Java Programs

---
+++ Swapno.java
@@ -0,0 +1,19 @@
+
+
+public class Swapno {
+	public static void main(String[]args) {
+		int a=10;
+		int b=20;
+		
+		//without using 3rd variable
+		
+		a =a+b;
+		b =a-b;
+		a =a-b;
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+		}
+
+}
